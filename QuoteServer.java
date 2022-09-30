d0e4e48490e21a149645b464232961c98440bd69
noahdaniels9 <80980047+noahdaniels9@users.noreply.github.com>
2022-09-29 20:23:50 -0700

    Add files via upload

---
+++ QuoteServer.java
@@ -0,0 +1,38 @@
+package project1;
+import java.net.*;
+import java.io.*;
+
+
+public class QuoteServer
+{
+	public static void main(String[] args){
+		try {
+			ServerSocket sock = new ServerSocket(6017);			
+			
+			/* now listen for connections */
+			while (true) {
+				Socket client = sock.accept();
+				
+				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+				
+				/* write the Date to the socket */
+				out.println(new java.util.Date().toString());
+
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+				String input;
+				
+				while((input = in.readLine()) != null) {
+					out.println(input);
+				}
+				/* close the socket and resume listening for connections */
+				client.close();
+				sock.close();
+				in.close();
+			}
+		}
+		catch (IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
+}
